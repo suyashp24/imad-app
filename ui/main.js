d0fdb59ab51d5f07ec1a96026f18f8bb29f725e0
suyashp24 <30681075+suyashp24@users.noreply.github.com>
2017-08-07 10:49:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ button.onClick = function(){
             if(request.status ===200){
                 var counter = request.responseText;
                 var span = document.getElementById('count');
-                span.innerHtml = counter.toString();
+                span.innerHTML = counter.toString();
             }
         }
     };
