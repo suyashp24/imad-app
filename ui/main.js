a9b59a50dd834a55660de5224da879f6981d5ec1
suyashp24 <30681075+suyashp24@users.noreply.github.com>
2017-08-07 10:43:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onClick = function(){
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.done){
+            if(request.status ===200){
+                var counter = request.responseText;
+            }
+        }
+    };
+    
+    request.open('GET','http://suyashpawar24.imad.hasura.io/counter',true);
+    request.send(null);
+};
